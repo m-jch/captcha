c1da41b699bb990fea234e359dbfab7f792d568b
Hieu Vo <hieu.vo@lazada.com>
2015-08-18 10:00:14 +0700

    improvement to add ability of choosing pool of charset

--- src/Mohsen/Captcha/Captcha.php
+++ src/Mohsen/Captcha/Captcha.php
@@ -6,15 +6,16 @@ use Symfony\Component\Security\Csrf\CsrfToken;
 
 class Captcha {
 
-    public static function getImage($count = null, $width = null, $height = null, $backgroundColor = null, $quality = null)
+    public static function getImage($count = null, $width = null, $height = null, $backgroundColor = null, $quality = null, $stringType = null)
     {
         $data = [];
-        $data['count'] = isset($count) ? $count : 7;
-        $data['width'] = isset($width) ? $width : 160;
-        $data['height'] = isset($height) ? $height : 70;
-        $data['backgroundcolor'] = isset($backgroundColor) ? $backgroundColor : 'efefef';
-        $data['quality'] = isset($quality) && ($quality > 100 || $quality < 0) ? $quality : 50;
+        $data['count'] = $count ?: 7;
+        $data['width'] = $width ?: 160;
+        $data['height'] = $height ?: 70;
+        $data['backgroundcolor'] = $backgroundColor ?: 'efefef';
+        $data['quality'] = $quality && ($quality > 100 || $quality < 0) ? $quality : 50;
         $data['csrf_token'] = static::generateCsrf();
+        $data['string_type'] = $stringType ?: 'mixedLowercase';
 
         $hashedUrl = Crypt::encrypt(http_build_query($data));
 
@@ -23,11 +24,10 @@ class Captcha {
 
     public static function create($hashedUrl)
     {
-        $url = Crypt::decrypt($hashedUrl);
-        parse_str($url, $url);
+        parse_str(Crypt::decrypt($hashedUrl), $url);
         static::checkCsrf($url['csrf_token']);
 
-        $captchaText = strtoupper(substr(Str::random(), 0, $url['count']));
+        $captchaText = static::getRandomString($url['string_type'], $url['count']);
         Session::put('captchaHash', Hash::make($captchaText));
 
         $image = imagecreate($url['width'], $url['height']);
@@ -38,10 +38,10 @@ class Captcha {
         $fixedFontSize = min($url['height'] / 2, $padding);
 
         for($i = 0; $i < $url['count']; $i++) {
-            $fontSize = mt_rand(60, 100) * $fixedFontSize / 100;
+            $fontSize = mt_rand(90, 130) * $fixedFontSize / 100;
 
             $x = $i * $padding + mt_rand(90, 140) * $padding / 100;
-            $y = $url['height'] / 2 + mt_rand(80, 100) * ($fontSize / 2) / 100;
+            $y = $url['height'] / 2 + mt_rand(100, 130) * ($fontSize / 2) / 100;
 
             $angle = mt_rand(5, 10);
 
@@ -80,5 +80,29 @@ class Captcha {
         } else {
             $csrfTokenManger->removeToken('login_csrf');
         }
+    }
+
+    public static function getRandomString($type, $count)
+    {
+        switch ($type) {
+            case 'alphabetOnly':
+                $pool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                break;
+            case 'numberOnly':
+                $pool = '0123456789';
+                break;
+            case 'mixedUppercase':
+                $pool = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                break;
+            case 'mixedLowercase':
+                $pool = '0123456789abcdefghijklmnopqrstuvwxyz';
+                break;
+            case 'mixed':
+            default:
+                $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                break;
+        }
+
+        return substr(str_shuffle(str_repeat($pool, $count)), 0, $count);
     }
 }
